8aaa8fb3f9d3e0ac77d3400c3c1cff4d462c10df
dwissaaj <dwisaji150@gmail.com>
2024-06-23 15:52:32 +0700

    add signup page

--- app/index.tsx
+++ app/index.tsx
@@ -1,5 +1,5 @@
 import BaseButton from "@/components/base/BaseButton";
-import { Link } from "expo-router";
+import { Link, router } from "expo-router";
 import { StatusBar } from "expo-status-bar";
 import { Image, ScrollView, Text, View, } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
@@ -8,14 +8,14 @@ export default function Index() {
   return (
     <SafeAreaView className="h-full bg-black">
       <ScrollView contentContainerStyle={{ height: '100%' }}>
-        <View className="bg-black w-full h-full justify-center items-center  px-4">
+        <View className="bg-black w-full min-h-full justify-center items-center  px-4">
           <Image source={require('../assets/images/landing/flower.png')} resizeMode="contain" className="w-full h-1/4 " />
           <View className="flex flex-row gap-3 ">
             <Text className="text-4xl font-fredbold text-white">Spud</Text>
             <Text className="text-4xl font-fredbold text-primary">Sprout</Text>
           </View>
           <Text className="text-md font-cabinreg text-white">From Seed to Harvest! Simplified</Text>
-          <BaseButton handlePress={() => {}} isLoading={false} containerStyles="w-full mt-7 bg-primary " textStyle="text-white" title="Let's Plan Together" />
+          <BaseButton handlePress={() => router.push('/login')} isLoading={false} containerStyles="w-full mt-7 bg-primary " textStyle="text-white" title="Let's Plan Together" />
         </View>
       </ScrollView>
       <StatusBar backgroundColor="" style="light" />
