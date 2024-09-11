fb5d10f7c33ee97c3036e8c04d94518342f87aa8
dwissaaj <dwisaji150@gmail.com>
2024-09-11 19:38:22 +0700

    add register page

--- app/(auth)/register/index.tsx
+++ app/(auth)/register/index.tsx
@@ -1,10 +1,15 @@
-import { View, Text } from 'react-native'
+import { View, Text, ScrollView } from 'react-native'
 import React from 'react'
+import { SafeAreaView } from 'react-native-safe-area-context'
 
 export default function Register() {
   return (
-    <View>
-      <Text>Register</Text>
-    </View>
+    <SafeAreaView className='h-full bg-black'>
+        <ScrollView>
+          <View className='w-full flex items-center p-2'>
+            <Text className='text-5xl font-fredbold text text-white'>Welcome to Garden! 🐝</Text>
+          </View>
+        </ScrollView>
+    </SafeAreaView>
   )
 }
