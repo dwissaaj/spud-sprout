5951d34d000367044139439e9ee9e453d0544c55
dwissaaj <dwisaji150@gmail.com>
2024-09-12 16:25:08 +0700

    add register page

--- app/(auth)/register/index.tsx
+++ app/(auth)/register/index.tsx
@@ -1,13 +1,29 @@
 import { View, Text, ScrollView } from 'react-native'
-import React from 'react'
+import React, { useState } from 'react'
 import { SafeAreaView } from 'react-native-safe-area-context'
+import FormField from '@/components/base/FormField'
+import BaseButton from '@/components/base/BaseButton'
 
 export default function Register() {
+  const [form, setForm] = useState({
+    email: '',
+    password: ''
+  })
   return (
     <SafeAreaView className='h-full bg-black'>
-        <ScrollView>
-          <View className='w-full flex items-center p-2'>
-            <Text className='text-5xl font-fredbold text text-white'>Welcome to Garden! 🐝</Text>
+        <ScrollView className='p-4 m-2 flex space-y-4'>
+          <View className='w-full flex items-center '>
+            <Text className='text-5xl font-fredbold text text-white'>Welcome to Garden!🐝</Text>
+          </View>
+          <View>
+            <Text className='text-white font-cbnreg text-xl'>Gardening is a way too relieve stress</Text>
+          </View>
+          <View className='mt-4 '>
+          <FormField onChangeText={(e) => setForm({...form, email: e})} keyboardType={'email-address'} value={form.email} title='Your Email' placeholder='Email Address' />
+          <FormField onChangeText={(e) => setForm({...form, password: e})} keyboardType={'email-address'} value={form.password} title='Your Password' placeholder='Secret' />
+          </View>
+          <View>
+            <BaseButton  title="Let's Go" isLoading={false} containerStyles='bg-white' textStyle=' font-fredbold' handlePress={() => console.log("Pressed")}/>
           </View>
         </ScrollView>
     </SafeAreaView>
